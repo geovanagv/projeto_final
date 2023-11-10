bcc4eb99f491e3a6395a37c00c7f7476e87fec7f
babicavalcante <142848871+babicavalcante@users.noreply.github.com>
2023-11-10 17:29:03 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,95 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Platform } from 'react-native';
 
-export default function Money() {
-    return(
-        <View Style={styles.container}>
-            <Text>Página Money!</Text>
-        </View>
-    );
+import { MaterialIcons } from '@expo/vector-icons'; 
+
+export default function CustomTabBar({ state, descriptors, navigation}) {
+  return (
+    <View style={styles.container}>
+
+      <View style={styles.content}>
+        {state.routes.map((route, index) => {
+            const { options } = descriptors[route.key];
+
+            const isFocused = state.index === index;
+
+            const onPress = () => {
+                const event = navigation.emit({
+                  type: 'tabPress',
+                  target: route.key,
+                  canPreventDefault: true,
+                });
+      
+                if (!isFocused && !event.defaultPrevented) {
+                  navigation.navigate(route.name, route.params);
+                }
+              };
+      
+              const onLongPress = () => {
+                navigation.emit({
+                  type: 'tabLongPress',
+                  target: route.key,
+                });
+              };
+
+              return(
+                <TouchableOpacity
+                  accessibilityRole="button"
+                  accessibilityState={isFocused ? { selected: true } : {}}
+                  accessibilityLabel={options.tabBarAccessibilityLabel}
+                  testID={options.tabBarTestID}
+                  onPress={onPress}
+                  onLongPress={onLongPress}
+                  style={styles.buttonTab}
+                >
+                    <View style={{ alignItems: 'center', padding: 4 }}>
+                        <View style={[styles.innerButton, { backgroundColor: isFocused ? "#f8e2fd" : "transparent" }]}>
+                            <MaterialIcons
+                              name={options.tabBarIcon}
+                              size={34}
+                              color={ isFocused ? "#8f2abd" : "#535353"}
+                            />
+                        </View>
+                    </View>
+                </TouchableOpacity>
+              )
+        })}
+
+      </View>
+
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+ container:{
+    justifyContent: 'center',
+    alignItems: 'center'
+ },
+ content: {
+    borderRadius: 99,
+    flexDirection: 'row',
+    marginBottom: Platform.OS === 'ios' ? 38 : 24,
+    alignItems: 'center', 
+    justifyContent: 'center',
+    position: "absolute",
+    bottom: 0,
+    backgroundColor: "#rgba(255,255,255, 0.9)",
+    gab: 8,
+    elevation: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+        width: 0,
+        height: 2
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 3.80
+ },
+ buttonTab: {
+    justifyContent: 'center',
+    alignItems: 'center'
+ },
+ innerButton: {
+    padding: 8, 
+    borderRadius: 99
+ }
 })
